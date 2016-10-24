e9b5cb8ca52d1bd3fc2dcf278b3ec47cc960aad5
julius2807 <julius_soe@yahoo.com>
2016-10-24 21:46:41 +0700

    Updated Comments

--- controllers/userController.js
+++ controllers/userController.js
@@ -12,7 +12,7 @@ module.exports = function(app, middleware, db) {
   	});
   });
 
-  // POST /users/login
+  // POST /users/login --> will generate Auth Token
   app.post('/users/login', function (req, res) {
   	var body = _.pick(req.body, 'email', 'password');
   	var userInstance;
